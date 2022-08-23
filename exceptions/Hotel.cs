fad4fb6dca7a295ca6820e2c2c2b22b939eb124a
Farid0558475484 <faridmh@code.edu.az>
2022-08-23 18:48:41 +0400

    ancag room classi yaratmisam

---
+++ exceptions/Hotel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace exceptions
+{
+    public class Hotel
+    {
+        Name 
+  Rooms array //içində Room obyektləri saxlayır və private-dır.
+  AddRoom() // Parametr olaraq Room obyekti qəbul edib rooms arrayinə əlavə edir.
+   Hotel class-ı üçün bir indexer yazılır və bir hotel obyekti yaratdıqdan sonra hotel[n] yazdığımız zaman rooms arrayindəki n-ci element geriyə qaytarılmalıdır, həm get etmək həmdə set etmək olmalıdır.
+    }
+}
---
+++ exceptions/Room.cs
@@ -0,0 +1,116 @@
+﻿using System;
+namespace exceptions
+{
+    public class Room
+    {
+
+
+
+        private int _id;      //qıraqdan set etmək olmayacaq yalnız get etmək olacaq və hər dəfə yeni bir Room obyekt yaradıldıqda(ctor-da) avtomatik bir vahid artacaq.
+        private string _name;
+        private float _price;
+        private int _personCapacity; //otağın neçə nəfərlik olduğunu bildirir.
+        private bool _isAvailable = true;    //otağın rezervasiya olunub olunmadığını göstərir, default olaraq true olmalıdır.
+
+
+
+        public int Id { get { return _id; } }
+        public Room()
+        {
+            _id++;
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != " ")
+                {
+                    _name = value;
+                }
+            }
+        }
+
+
+
+        public float Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public int PersonCapacity
+        {
+            get
+            {
+                return _personCapacity;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _personCapacity = value;
+                }
+            }
+        }
+
+
+        public bool IsAvailable
+        {
+            get
+            {
+                return _isAvailable;
+            }
+
+            set
+            {
+                if (_isAvailable == true)
+                {
+                    _isAvailable = true;
+                }
+            }
+        }
+
+
+
+
+        public string ShowInfo()
+        {
+            return ("Room Price " + Price + "Person Capacity " + PersonCapacity + " Room Name " + IsAvailable);
+        }
+
+
+
+        public override string ToString()
+        {
+            return ShowInfo();
+        }
+
+
+        public Room(int Id, string Name, float Price, int PersonCapacit, bool IsAvailable)
+        {
+            Id = _id;
+            Name = _name;
+            PersonCapacity = _personCapacity;
+            IsAvailable = _isAvailable;
+        }
+    }
+
+
+
+}
